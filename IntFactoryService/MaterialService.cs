2a46c54981d06965c53df1828c9dbd78a84a959d
sunallen521 <sunallen@163.com>
2016-03-23 11:36:43 +0800

    逻辑变更

---
+++ IntFactoryService/MaterialService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntFactoryService
+{
+    /// <summary>
+    /// 材料服务层
+    /// </summary>
+    public class MaterialService
+    {
+
+    }
+}
