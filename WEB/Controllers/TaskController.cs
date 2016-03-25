739c8d28db18620ca0cb21dc895b41eae319e3fd
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-03-25 17:49:45 +0800

    调整跳转

--- WEB/Controllers/TaskController.cs
+++ WEB/Controllers/TaskController.cs
@@ -13,7 +13,7 @@ namespace YXERP.Controllers
     public class TaskController : BaseController
     {
         // GET: /Task/
-        string token = "f30b67c1-2577-4eb8-8932-b4ba7dad0efe";
+        string token = "3092f615-9aef-4288-8129-417a1d1ff46b";
         string refreshToken = "be462dcd-1baf-4665-8444-1646d8350c8c";
         #region view
         public JsonResult batchUpdateFent()
