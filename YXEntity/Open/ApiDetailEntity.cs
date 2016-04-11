e9d64d849aeb3f71d680103247586408499d14dd
muge <xiaoxiaomuge@163.com>
2016-04-11 22:48:02 +0800

    新增开放平台接口逻辑

---
+++ YXEntity/Open/ApiDetailEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntFactoryEntity
+{
+    public class ApiDetailEntity
+    {
+        public int AutoID;
+
+        public string ApiID;
+
+        public string ModuleID;
+
+        public string Name;
+
+        public string Remark;
+
+        public string Detail;
+
+        public int Status;
+
+        public int Sort;
+
+        public DateTime CreateTime;
+    }
+}
---
+++ YXEntity/Open/ApiModuleEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntFactoryEntity
+{
+    public class ApiModuleEntity
+    {
+        public int AutoID;
+
+        public string ModuleID;
+
+        public string Name;
+
+        public string Remark;
+
+        public string Icon;
+
+        public int Status;
+
+        public int Sort;
+
+        public DateTime CreateTime;
+
+        public List<ApiDetailEntity> Details;
+
+        public void FillData(System.Data.DataRow dr)
+        {
+            dr.FillData(this);
+        }
+    }
+}
