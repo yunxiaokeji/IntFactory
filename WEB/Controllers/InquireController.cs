1bd85954737e3aa93da8209831546c226a383ed5
song19940906 <2079973693@qq.com>
2016-05-10 17:43:02 +0800

    5.10添加客户查询订单状态页面

--- WEB/Controllers/InquireController.cs
+++ WEB/Controllers/InquireController.cs
@@ -16,5 +16,70 @@ namespace YXERP.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 判断手机号是否存在
+        /// </summary>
+        /// <param name="loginName">手机号码</param>
+        /// <returns></returns>
+        public JsonResult IsExistLoginName(string loginPhone)
+        {
+            bool bl = IntFactoryBusiness.OrganizationBusiness.IsExistLoginName(loginPhone);
+            Dictionary<string, object> JsonDictionary = new Dictionary<string, object>();
+            JsonDictionary.Add("Result", bl ? 1 : 0);
+
+            return new JsonResult()
+            {
+                Data = JsonDictionary,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        /// <summary>
+        /// 发送手机验证码
+        /// </summary>
+        /// <param name="mobilePhone">手机号码</param>
+        /// <returns></returns>
+        public JsonResult SendMobileMessage(string mobilePhone)
+        {
+            Dictionary<string, object> JsonDictionary = new Dictionary<string, object>();
+            Random rd = new Random();
+            int code = rd.Next(100000, 1000000);
+
+            bool flag = Common.MessageSend.SendMessage(mobilePhone, code);
+            JsonDictionary.Add("Result", flag ? 1 : 0);
+
+            if (flag)
+            {
+                Common.Common.SetCodeSession(mobilePhone, code.ToString());
+
+                Common.Common.WriteAlipayLog(mobilePhone + " : " + code.ToString());
+
+            }
+
+            return new JsonResult()
+            {
+                Data = JsonDictionary,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        /// <summary>
+        /// 验证手机验证码
+        /// </summary>
+        /// <param name="mobilePhone"></param>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public JsonResult ValidateMobilePhoneCode(string mobilePhone, string code)
+        {
+            bool bl = Common.Common.ValidateMobilePhoneCode(mobilePhone, code);
+            Dictionary<string, object> JsonDictionary = new Dictionary<string, object>();
+            JsonDictionary.Add("Result", bl ? 1 : 0);
+
+            return new JsonResult()
+            {
+                Data = JsonDictionary,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
     }
 }
---
+++ WEB/modules/scripts/inquireorder/inquireorder.js
@@ -0,0 +1,83 @@
+﻿define(function (require, exports, module) {
+    var Global = require("global");
+
+    var InquireOrder = {};
+
+    InquireOrder.init = function () {
+        InquireOrder.bindEvent();
+    };
+
+    InquireOrder.bindEvent = function () {
+        
+        //发送验证码
+        $(".inquire-form-btn").click(function () {
+            var phoneLogin=$(".inquire-form-phone").val();
+            if (phoneLogin == '') {
+                alert("手机号不能为空");
+                return;
+            }
+            else {
+                if (Global.validateMobilephone(phoneLogin)) {
+                    InquireOrder.SendMobileMessage("inquire-form-btn", phoneLogin);
+                }
+                else {
+                    alert("手机号格式有误");
+                }
+            }
+        });
+
+        //发送手机验证码
+        var timeCount = 60;
+        var interval = null;
+        InquireOrder.SendMobileMessage = function (id, mobilePhone) {
+            var $btnSendCode = $("." + id);
+            $btnSendCode.attr("disabled", "disabled");
+
+            $("." + id).css("background-color", "#aaa");
+            interval = setInterval(function () {
+                var $btnSendCode = $("." + id);
+                timeCount--;
+                $btnSendCode.val(timeCount + "秒后重发");
+                $btnSendCode.text("发送中...");
+                $(".inquire-form-button").css({"background-color":"#007aff","color":"#fff","font-size":"22px"});
+                
+                if (timeCount == 0) {
+                    clearInterval(interval);
+                    timeCount = 60;
+                    $btnSendCode.val("获取验证码").css("background-color", "#4a98e7");
+                    $btnSendCode.removeAttr("disabled");
+                    $btnSendCode.text("获取验证码");
+                    $(".inquire-form-button").css({ "background-color": "#d5d5d5", "color": "#666", "font-size": "12px" });
+                    
+                }
+
+            }, 1000);
+
+            $.post("/Inquire/SendMobileMessage", { mobilePhone: mobilePhone }, function (data) {
+                if (data.Result == 0) {
+                    alert("验证码发送失败");
+                }
+            });
+        }
+
+        //验证手机验证码
+        $(".inquire-form-button").click(function () {
+            var phoneLogin = $(".inquire-form-phone").val();
+            var phonevalidation = $(".inquire-form-numb").val();
+            if (phoneLogin==""||phonevalidation=="") {
+                alert("手机号或验证码不能为空");
+            } else {
+                $.post("/Inquire/ValidateMobilePhoneCode", {mobilePhone:phoneLogin,code:phonevalidation}, function (data) {
+                    if (data.Result == 0) {
+                        alert("验证码有误");
+                    } else {
+                        alert("查询成功");
+                        
+                    }
+                });
+            }
+        });
+        
+    }
+    module.exports = InquireOrder;
+});
