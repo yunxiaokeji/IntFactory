922d4bb5485200c13fe9c42ac648684e099950a3
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-05-13 17:16:44 +0800

    调整页面

--- WEB/modules/scripts/inquireorder/inquireorder.js
+++ WEB/modules/scripts/inquireorder/inquireorder.js
@@ -10,8 +10,6 @@
     InquireOrder.isloading = true;
 
     InquireOrder.bindEvent = function () {
-        
-        
         //发送验证码
         $(".inquire-form-btn").click(function () {
             var mobilePhone = $(".inquire-form-phone").val();
@@ -28,7 +26,7 @@
                             alert("验证码发送失败");
                         }
                         else {
-                            $(".inquire-form-button").css({ "background-color": "#007aff", "color": "#fff", "font-size": "22px" }).removeAttr("disabled");
+                            $(".inquire-form-button").css({ "background-color": "#4a98e7", "color": "#fff" }).removeAttr("disabled");
                             InquireOrder.SendMobileMessage("inquire-form-btn", mobilePhone);
                         }
                     });
@@ -40,34 +38,7 @@
             }
         });
 
-        //发送手机验证码
-        var timeCount = 60;
-        var interval = null;
-        InquireOrder.SendMobileMessage = function (id, mobilePhone) {
-            var $btnSendCode = $("." + id);
-            $btnSendCode.attr("disabled", "disabled");
-
-            $("." + id).css("background-color", "#aaa");
-            interval = setInterval(function () {
-                var $btnSendCode = $("." + id);
-                timeCount--;
-                $btnSendCode.val(timeCount + "秒后重发");
-               
-                if (timeCount == 0) {
-                    clearInterval(interval);
-                    timeCount = 60;
-                    $btnSendCode.val("获取验证码").css("background-color", "#4a98e7");
-                    $btnSendCode.removeAttr("disabled");
-                    $btnSendCode.text("获取验证码");                   
-                    
-                }
-
-            }, 1000);
-
-           
-        }
-
-        //验证手机验证码
+        //查询进度
         $(".inquire-form-button").click(function () {
             var phoneLogin = $(".inquire-form-phone").val();
             var phonevalidation = $(".inquire-form-numb").val();
@@ -75,7 +46,7 @@
                 Global.post("/Inquire/ValidateMobilePhoneCode", { mobilePhone: phoneLogin, code: phonevalidation }, function (code) {
                     if (code.Result == 0) {
                         alert("验证码有误");
-                        $(".inquire-form-button").attr("disabled", "disabled").css("background", "#d5d5d5");
+                        //$(".inquire-form-button").attr("disabled", "disabled").css("background", "#d5d5d5");
                     } else {
                         $(".img-loading").remove();
                         $(".info").remove();
@@ -83,48 +54,68 @@
                     }
                 });
 
-                $(".inquire-form-button").click(function () {
-                    $(".img-loading").remove();
-                    $(".info").remove();
-                    InquireOrder.getOrderByPhone();
+                //$(".inquire-form-button").click(function () {
+                //    $(".img-loading").remove();
+                //    $(".info").remove();
+                //    InquireOrder.getOrderByPhone();
 
-                });
-
-                //通过键盘触发事件
-                //$(document).bind('keydown', function (e) {
-                //    if (e.which == 13) {
-                //        $(".img-loading").remove();
-                //        $(".info").remove();
-                //        InquireOrder.getOrderByPhone();
-                //    }
                 //});
 
             } else {
                 alert("手机号或验证码不能为空");
-                $(".inquire-form-button").attr("disabled", "disabled").css("background", "#d5d5d5");
+                //$(".inquire-form-button").attr("disabled", "disabled").css("background", "#d5d5d5");
             }
-            
-            
+             
         });
 
 
+        //$(document).keydown(function (e) {
+        //    if (e.keyCode === 13) {
+        //        $(".inquire-form-button").click();
+        //    }
+        //});
+    }
+
+    //发送手机验证码
+    var timeCount = 60;
+    var interval = null;
+    InquireOrder.SendMobileMessage = function (id, mobilePhone) {
+        var $btnSendCode = $("." + id);
+        $btnSendCode.attr("disabled", "disabled");
+
+        $("." + id).css("background-color", "#aaa");
+        interval = setInterval(function () {
+            var $btnSendCode = $("." + id);
+            timeCount--;
+            $btnSendCode.val(timeCount + "秒后重发");
+
+            if (timeCount == 0) {
+                clearInterval(interval);
+                timeCount = 60;
+                $btnSendCode.val("获取验证码").css("background-color", "#4a98e7");
+                $btnSendCode.removeAttr("disabled");
+                $btnSendCode.text("获取验证码");
+
+            }
+
+        }, 1000);
 
 
-        
     }
 
     InquireOrder.getOrderByPhone = function () {
-        if (InquireOrder.isloading==true) {
+        if (InquireOrder.isloading)
+        {
             InquireOrder.isloading = false;
             $(".inquire").after('<div class="img-loading"><img style="width:20px;" src="/modules/images/ico-loading.gif" /></div>');
             var mobilePhone = $(".inquire-form-phone").val();
+
             Global.post("/Inquire/InquireOrderByPhone", { mobilePhone: mobilePhone }, function (data) {
                 if (data.items.length > 0) { 
                     doT.exec("/template/inquireorder/inquireorder.html", function (template) {
                         var innerhtml = template(data.items);
                         innerhtml = $(innerhtml);
                         $(".inquire").after(innerhtml);
-
 
                         for (var i = 0; i < data.items.length; i++) {
                             var item = data.items[i];
@@ -146,13 +137,15 @@
                         }
                     });
                     InquireOrder.isloading = true;
-                } else {
+                }
+                else {
                     $(".inquire").after('<div class="info"><div class="infobox row"><div class="info-none"></div></div></div>');
                     InquireOrder.isloading = true;
                 }
                 $(".img-loading").remove();
             });
-        } else {
+        }
+        else {
             alert("数据正在加载，请稍等");
         }
          
