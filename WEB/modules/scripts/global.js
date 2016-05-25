4e8d7c1cbc0a61b71907d52355bca6be1f9edbe2
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-05-25 14:07:30 +0800

    优化任务

--- WEB/modules/scripts/global.js
+++ WEB/modules/scripts/global.js
@@ -223,5 +223,27 @@
         return str;
     }
 
+    Global.setCookie=function(name, value) {
+        var Days = 30;
+        var exp = new Date();
+        exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+        document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+    }
+
+    Global.getCookie=function(name) {
+        var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+        if (arr = document.cookie.match(reg))
+            return unescape(arr[2]);
+        else
+            return null;
+    }
+
+    Global.delCookie=function(name) {
+        var exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        var cval = getCookie(name);
+        if (cval != null)
+            document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+    }
     module.exports = Global;
 });
