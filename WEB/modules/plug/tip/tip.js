916ddeeb72348d18299054861eb1dab90ab6921c
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-06-01 14:44:24 +0800

    首页调整

--- WEB/modules/plug/tip/tip.js
+++ WEB/modules/plug/tip/tip.js
@@ -12,26 +12,32 @@
             var _this = $(this);
             
             _this.mouseover(function (event) {
-                
-                //$(".content-tip").remove();
-                //var e = event || window.event;
-                //var tipDiv = createTip();
-                //tipDiv = $(tipDiv);
-                //$("body").append(tipDiv);
+                var e = event || window.event;
+                var tipDiv = createTip();
+                var height = tipDiv.find(".tip-msg").height();
+                var thisWidth = _this.width();
 
-                //var height = tipDiv.find(".tip-msg").height();
-                //tipDiv.css({ "top": (e.clientY + document.body.scrollTop - height - 30) + "px", "left": (e.clientX - Option.width / 2) + "px" });
-                //tipDiv.find(".tip-lump").css({ "top": (height+ 19) + "px", "left": (Option.width / 2 - 3) + "px" });
+                tipDiv.css({ "top": (e.clientY + document.body.scrollTop - height - 30) + "px", "left": (_this .offset().left- ((Option.width - thisWidth) / 2)) + "px" });
+                tipDiv.find(".tip-lump").css({ "top": (height+ 19) + "px", "left": (Option.width / 2 - 3) + "px" });
             });
 
         }
 
         var createTip = function () {
-            var html = '<div class="content-tip">';
-            html += '<div class="tip-msg" style="width:'+Option.width+'px">' + Option.msg + '</div>';
-            html += '<div class="tip-lump"></div>';
-            html += '</div>';
-            return html;
+            if ($(".content-tip").length == 1) {
+                $(".content-tip").find(".tip-msg").html(Option.msg).css("width",Option.width+"px");
+
+                return $(".content-tip");
+            }
+            else {
+                var html = '<div class="content-tip">';
+                html += '<div class="tip-msg" style="width:' + Option.width + 'px">' + Option.msg + '</div>';
+                html += '<div class="tip-lump"></div>';
+                html += '</div>';
+                html = $(html);
+                $("body").append(html);
+                return html;
+            }
         }
         
     })(jQuery)
