255e94261dfb0d1d77f1f32a46bba848279bc80a
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-06-20 15:33:08 +0800

    调整

--- WEB/modules/scripts/task/detail.js
+++ WEB/modules/scripts/task/detail.js
@@ -1,10 +1,10 @@
 ﻿define(function (require, exports, module) {
     var DoT = require("dot");
     var Global = require("global");
-    var TalkReply = require("scripts/task/reply"); 
     var Upload = require("upload");
     var Easydialog = require("easydialog");
-    var Pager=require("pager");
+    var Pager = require("pager");
+    var TalkReply = null;
     var ChooseUser = null;
     var CutoutDoc = null;
     var SewnDoc = null;
@@ -95,6 +95,8 @@
             ProcessDYCosts.initProcessCosts(ObjectJS.orderid, Global, DoT, ObjectJS.orderType);
         }
 
+        TalkReply = require("scripts/task/reply");
+        TalkReply.initTalkReply(ObjectJS, 'task');
 
         $(".part-btn").hide();
 
@@ -150,11 +152,6 @@
         //绑定任务样式图
         ObjectJS.bindOrderImages();
 
-        //初始化任务讨论列表
-        var callBack = function () {
-            $(".module-tab li.default-check").click();
-        };
-        TalkReply.initTalkReply(ObjectJS, 'task', callBack);
         //任务模块切换
         $(".module-tab li").click(function () {
             var _this = $(this);
@@ -184,6 +181,12 @@
                     _this.data("isget", "1");
                 }
             }
+            else if (_this.data("id") == "taskReplys") {
+                if (!_this.data("isget")) {
+                    TalkReply.getTaskReplys(1);
+                    _this.data("isget", "1");
+                }
+            }
             else if (_this.data("id") == "navCutoutDoc") {
                 CutoutDoc.getCutoutDoc();
             }
@@ -197,6 +200,8 @@
                 SendDYOrders.getSendDYDoc();
             }
         });
+
+        $(".module-tab li.default-check").click();
 
         //标记任务完成
         if ($("#FinishTask").length == 1) {
