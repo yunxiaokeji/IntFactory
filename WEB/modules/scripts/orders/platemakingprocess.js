6c828186577b993bae71ba43ee81579acdeead8c
song19940906 <2079972693@qq.com>
2016-07-05 15:17:02 +0800

    调整

--- WEB/modules/scripts/orders/platemakingprocess.js
+++ WEB/modules/scripts/orders/platemakingprocess.js
@@ -21,7 +21,7 @@
         if (img == "") {
             img = "/modules/images/none-img.png";
         };
-
+        Objects.removeTaskPlateOperate();
         //工艺说明录入上传附件
         Upload.uploader({
             browse_button: 'upLoadOneImg',
