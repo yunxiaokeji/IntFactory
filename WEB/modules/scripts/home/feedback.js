0a2558f7709f332783b1e0b09c19cd7da95a1936
w609365252 <609365252@qq.com>
2016-07-05 11:22:07 +0800

    7.4 七牛

--- WEB/modules/scripts/home/feedback.js
+++ WEB/modules/scripts/home/feedback.js
@@ -9,29 +9,6 @@
 
     ObjectJS.bindEvent = function () {
 
-        //ProductIco = Upload.createUpload({
-        //    element: "upload",
-        //    buttonText: "选择附件",
-        //    className: "",
-        //    multiple: false,
-        //    data: { folder: '', action: 'add', oldPath: "" },
-        //    success: function (data, status) {
-        //        if (data.Items.length > 0) {
-        //            for (var i = 0; i < data.Items.length; i++) {
-        //                if ($(".feed-annex div:last-child li").length < 5) {
-        //                    var img = $('<li><img src="' + data.Items[i] + '" /><i class="iconfont">&#xe620;</i></li>');
-        //                    $(".feed-annex div:last-child ul").html(img);
-        //                    img.find(".iconfont").click(function () {
-        //                        $(this).parent().remove();
-        //                    });
-        //                }
-        //            }
-        //        } else {
-        //            alert("只能上传jpg/png/gif类型的图片，且大小不能超过5M！");
-        //        }
-        //    }
-        //});
-
         var upload = Upload.uploader({
             browse_button: 'upload',
             container: 'feedback-file',
@@ -39,7 +16,7 @@
             file_path: "/Content/UploadFiles/FeedBack/",
             successItems: '#feed-images li',
             picture_container: 'feed-images',
-            maxQuantity: 5,
+            maxQuantity: 1,
             maxSize: 5,
             fileType: 1,
             init: {}
