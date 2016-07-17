9de9b0bbacb515fb6399c51e012eedb321a35810
song19940906 <2079972693@qq.com>
2016-07-17 14:02:56 +0800

    7.17 selectlogn头部

--- WEB/Controllers/DefaultController.cs
+++ WEB/Controllers/DefaultController.cs
@@ -66,7 +66,7 @@ namespace YXERP.Controllers
             else
             {
                 return Redirect("/Default/Index");
-            }            
+            }               
         }
 
         public ActionResult SettingHelp()
