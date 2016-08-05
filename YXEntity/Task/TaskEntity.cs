9f2f7653b26428196964a6ae56226adf0b4fb501
w609365252 <609365252@qq.com>
2016-08-05 10:10:34 +0800

    8.4智能工厂功能调整

--- YXEntity/Task/TaskEntity.cs
+++ YXEntity/Task/TaskEntity.cs
@@ -89,8 +89,6 @@ namespace IntFactoryEntity.Task
 
         public List<TaskMember> TaskMembers { get; set; }
 
-        public List<CategoryItemsEntity> TaskDescs { get; set; }
-
         /// <summary>
         /// 预警状态 0：正常 1：快到期 2：已超期
         /// </summary>
