b98058001b36c291e919d82d62f938f429a52696
song19940906 <2079973693@qq.com>
2016-08-18 16:58:51 +0800

    8.18 智能工厂--制版

--- WEB/modules/scripts/orders/platemakingprocess.js
+++ WEB/modules/scripts/orders/platemakingprocess.js
@@ -134,6 +134,7 @@
     //删除行操作按钮(制版工艺)
     Objects.removeTaskPlateOperate = function () {
         $("span.ico-dropdown").remove();
+        $("#Platemak table").find("tr td ").css("line-height","28px");
         $("#Platemak table").find("tr:first").addClass("fontbold");
         $("#Platemak table").find("tr:first").find("td").css({ "border-top": "0", "border-bottom": "1px solid", "font-size": "16px" });
         $("#Platemak table").find("tr").find("td").removeClass("tLeft");
@@ -146,8 +147,7 @@
         $("#Platemak table").find("tr:first").find("td:last").css("margin-left", "10%");
     };
 
-    Objects.getOrderRemork = function (order) {
-        
+    Objects.getOrderRemork = function (order) {        
         var tableModel = {}, xattr = [], yattr = [],xyattr=[],xy=[];
         for (var i = 0; i < order.OrderGoods.length; i++) {            
             if ($.inArray(order.OrderGoods[i].XRemark, xattr) == -1 ) {
